59823904a2b141d4cf9ff48ffbc1999e10c7f2b1
hlaingwinphyoe <hlaingwinphyoedev@gmail.com>
2022-02-26 10:58:24 +0630

    first

---
+++ js/app.js
@@ -0,0 +1,57 @@
+filterObjects("pathologist");
+
+function filterObjects(c){
+    let x, i;
+    x = document.getElementsByClassName("box");
+    if(c === "all") c = "";
+    for(i=0;i<x.length;i++){
+        removeClass(x[i], "show");
+        if(x[i].className.indexOf(c) > -1) addClass(x[i], "show")
+    }
+}
+
+function addClass(element,name){
+    let i,arr1,arr2;
+    arr1 = element.className.split(" ")
+    arr2 = name.split(" ");
+    for(i=0;i<arr2.length;i++){
+        if(arr1.indexOf(arr2[i]) === -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name){
+    let i,arr1,arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i=0; i<arr2.length;i++){
+        while(arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]),1)
+        }
+    }
+    element.className = arr1.join(" ");
+}
+$("#button .filterButton").click(function (){
+    $(this).addClass("active").siblings().removeClass("active");
+});
+
+const toTop = document.querySelector(".scroll_to_top");
+window.addEventListener("scroll", ()=> {
+    if(window.pageYOffset > 100){
+        toTop.classList.add("scrollTop");
+    }
+    else{
+        toTop.classList.remove("scrollTop");
+    }
+});
+
+new WOW().init();
+
+$(window).on("load",function (){
+    $(".loader-container").fadeOut(500,function (){
+        $(this).remove();
+    });
+});
+
+
